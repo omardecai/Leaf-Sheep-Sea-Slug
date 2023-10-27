256fdd63c0248a4a7cdf26234d1dbf0c1c1e8ac1
omardecai <149150047+omardecai@users.noreply.github.com>
2023-10-27 16:29:38 -0600

    Update Dungeon.java

---
+++ Dungeon.java
@@ -0,0 +1,38 @@
+/**
+ * This is the Dungeon class that sets and gets the dungeon map from the utility class
+ * and prints the dungeon map
+ * @author Edgar Arellano
+ */
+class Dungeon {
+
+    private String[][] map;
+
+    /**
+     * This method sets the dungeonMap gathered from the Utility Class
+     * @param dungeonMap
+     */
+    public void setMap(String[][] dungeonMap) {
+        map = dungeonMap;
+    }
+
+    /**
+     * Getter method to return the map
+     * @return this method returns the dungeon map
+     */
+    public String[][] getMap(){
+        return map;
+    }
+
+    /**
+     * Print method to print out the dungeon map
+     * 
+     */
+    public void printMap() {
+        for (String[] row : map) {
+            for (String cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
