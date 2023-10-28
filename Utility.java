ba5d2fa431284af7206c876aba1d79a60d6be38e
omardecai <149150047+omardecai@users.noreply.github.com>
2023-10-27 18:01:29 -0600

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -5,20 +5,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The Utility class contains methods to read files 'Dungeon.csv' and 'users.csv'
+ * The Utility class contains methods to read files 'Dungeon.csv' and 'users.csv' and methods that 
+ * check the username and passwordinputed by the user. If no user is found
+ * another method will allow the user to register 
  * @author Edgar Arellano
+ * 
  */
 class Utility {
-    
-    
+    private List<String[]> userDataArray;
+
+    public void userDataFile(String userFile){
+        this.userDataArray = readUserInfo(userFile);
+    }
+
     /**
-     * 
-     * @param file
+     * This method reads the dungeon file "Dungeon.csv" and stores into an array
+     * @param dungeonFile
      * @return this method returns the map after reading the 'Dungeon.csv' file
      */
-    public String[][] readDungeon(String file) {
+    public String[][] readDungeon(String dungeonFile) {
         List<String[]> dungeonMap = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(dungeonFile))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
@@ -36,36 +43,65 @@ class Utility {
     }
 
         /**
-         * This method reads the file 'Users.csv' and checks the username and password entered by the user
-         * @param filePath
-         * @param targetUsername
-         * @param targetPassword
-         * @return this method returns a boolean after reading the users file and checking for password and username
+         * This method reads the file 'Users.csv' and stores in arrayList
+         * @param userFile
          */
-     public static boolean readUserInfo(String filePath, String targetUsername, String targetPassword){
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+     public List<String[]> readUserInfo(String userFile){
+        List<String[]> userData = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(userFile))){
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] userInfo = line.split(",");
-                if (userInfo.length >= 3) {
-                    String username = userInfo[2];
-                    String password = userInfo[5];
-
-                    if (username.equals(targetUsername)&& password.equals(targetPassword)) {
-                        return true;
-                    }
-                    
-                }
-                
-            }
-            
+                userData.add(userInfo);
+             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return userData;
+    }
+
+    /**
+     * This method checks the username and password entered by the player and returns a boolean upon 
+     * success or failure
+     * 
+     * @param targetUsername
+     * @param targetPassword
+     * @return boolean depending on successful user and password
+     */
+     public boolean checkUser(String targetUsername, String targetPassword){
+        for (String[] userData : userDataArray) {
+            if (userData.length == 11) {
+                String username = userData[2];
+                String password = userData[5];
+                
+                if (username.equals(targetPassword)&& password.equals(targetPassword)) {
+                    return true;
+                }
+            }
+        }
         return false;
      }
-
+    /**
+     * Allows the user to register as new user with the parameters as inputs
+     * 
+     * @param state
+     * @param username
+     * @param firstname
+     * @param password
+     * @param lastName
+     * @param city
+     * @param zip
+     * @param dateOfBirth
+     */
+     public void registerUser(String state, String username, String firstname, String password, String lastName, String city, String zip, String dateOfBirth){
+        String newEntry = state + ",0," + username + firstname + ",0," + password + lastName + city + zip + dateOfBirth;
+        userDataArray.add(newEntry.split(","));
+     }
+     /**
+      * Creates a saveFile for the game
+      * @param saveFile
+      */
      public void Savegame(String[] saveFile){
-        
+
      }
 }
