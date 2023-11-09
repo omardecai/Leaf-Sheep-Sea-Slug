effc85b4695969cd611dd180967fa2729d3bfd5a
Treyah <holguinfj@gmail.com>
2023-11-08 17:16:34 -0700

    Updated User.java to include attributes: battlesWon, battlesLost, itemTotalNum, userGameCompletions.

--- User.java
+++ User.java
@@ -3,17 +3,17 @@
  * important information of the user logged in. 
  *
  * @author Trey Holguin
- * @version 1.1 
+ * @version 2.0
  */
 public class User{
     
     private String username = "unknown";
     private String pin;
     private String lastSignIn;
+    private int battlesWon, battlesLost, itemTotalNum, userGameCompletions;
 
-    
     /** 
-     * Sets the user's Username which will be referenced in RunGame.java
+     * Setter method for the user's Username which will be referenced in RunGame.java
      * @param usernameSetter
      */
     public void setUsername(String usernameSetter){
@@ -21,7 +21,7 @@ public class User{
     }
     
     /** 
-     * Sets the user's PIN which will be referenced in RunGame.java
+     * Setter method for the user's PIN which will be referenced in RunGame.java
      * @param pinSetter
      */
     public void setPin(String pinSetter){
@@ -29,13 +29,44 @@ public class User{
     }
     
     /** 
-     * 
+     * Setter method that sets the user's last login. 
      * @param LSI sets the user's last sign in time as a String.
      */
     public void setLastSignIn(String LSI){
         this.lastSignIn = LSI;
     }
     
+    /**
+     * Setter method for battlesWon attribute. 
+     * @param won 
+     */
+    public void setBattlesWon(int won){
+        this.battlesWon = won;
+    }
+
+    /**
+     * Setter method for the battlesLost attribute. 
+     * @param lost
+     */
+    public void setBattlesLost(int lost){
+        this.battlesLost = lost;
+    }
+
+    /**
+     * Sets the amount of items that the user has collected within the game.
+     * @param items int value representing the user's total collect items. 
+     */
+    public void setItemTotalNum(int items){
+        this.itemTotalNum = items;
+    }
+
+    /**
+     * Will keep track of the amount of times the user has completed the game.
+     * @param completions Number of the times the game has been completed 
+     */
+    public void setGameCompletions(int completions){
+        this.userGameCompletions = completions; 
+    }
     /** 
      * Getter method for obtaining user's PIN. 
      * @return username as a String
@@ -57,6 +88,38 @@ public class User{
      */
     public String getLastSignIn() {
         return lastSignIn;
+    }
+
+    /**
+     * Getter method for the amount of battles the user has won. 
+     * @return int value of battles won.
+     */
+    public int getBattlesWon(){
+        return battlesWon; 
+    }
+
+    /**
+     * Getter method for the amount of battles the user has lost.
+     * @return int value of battles lost. 
+     */
+    public int getBattlesLost(){
+        return battlesLost;
+    }
+
+    /**
+     * Getter method for the amount of items the user has collected.
+     * @return int value of the total items. 
+     */
+    public int getItemTotalNum(){
+        return itemTotalNum; 
+    }
+
+    /**
+     * Getter method for the amount of times the user has completed the game. 
+     * @return int value of the number of times user has completed game. 
+     */
+    public int getGameCompletions(){
+        return userGameCompletions; 
     }
 
 }
